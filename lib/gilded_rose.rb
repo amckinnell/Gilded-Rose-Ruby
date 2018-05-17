4d4d443aed8747bf51a9641710f5e98de725bec7
Cindy Green <cimigreen@hotmail.com>
2018-05-17 09:59:23 -0400

    not passing class variables explicitly

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,16 +8,18 @@ class GildedRose
   end
 
   def tick
-    before_sell_by(@name, @quality)
     if @name != "Sulfuras, Hand of Ragnaros"
+      before_sell_by
       @days_remaining = @days_remaining - 1
     end
     if @days_remaining < 0
-      after_sell_date(@name, @quality)
+      after_sell_date
     end
   end
 
-  def after_sell_date(name, quality)
+  private 
+  
+  def after_sell_date
     if @name != "Aged Brie"
       if @name != "Backstage passes to a TAFKAL80ETC concert"
         if @quality > 0
@@ -37,31 +39,40 @@ class GildedRose
     end
   end
 
-  def before_sell_by(name, quality)
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name == "Conjured Mana Cake"
-          @quality = @quality - 2
-        elsif @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
+  def quality_increases
+    if @quality < 50
+      @quality = @quality + 1
+      if @name == "Backstage passes to a TAFKAL80ETC concert"
+        if @days_remaining < 11
+          if @quality < 50
+            @quality = @quality + 1
           end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
+        end
+        if @days_remaining < 6
+          if @quality < 50
+            @quality = @quality + 1
           end
         end
       end
     end
+  end
+
+
+  def before_sell_by
+    if quality_decreases?
+      if @quality > 0
+        if @name == "Conjured Mana Cake"
+          @quality = @quality - 2
+        else
+          @quality = @quality - 1
+        end
+      end
+    else
+      quality_increases(@name, @quality)
+    end
+  end
+
+  def quality_decreases?
+    @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
   end
 end
